3f4e9d43fb8a567cfc581bf450c15515feaed9b1
林满佳 <tywf91@gmail.com>
2014-10-10 18:02:45 +0800

    提交代码

--- lib/qiniu_mini_sdk.rb
+++ lib/qiniu_mini_sdk.rb
@@ -3,6 +3,7 @@ require "qiniu_mini_sdk/version"
 module QiniuMiniSdk
   # Your code goes here...
   autoload :Policy, 'qiniu_mini_sdk/policy'
+  autoload :Sign, 'qiniu_mini_sdk/sign'
 
   def self.setup
     QiniuMiniSdk.default
